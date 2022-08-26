9375db3371f5faac4ea621311aa05e120b09fc61
Jan Erik <jan.johannessen@bouvet.no>
2022-08-26 20:14:23 +0200

    new update

--- global_temp/web_scrap.py
+++ global_temp/web_scrap.py
@@ -3,7 +3,7 @@ import urllib.request as r
 response = r.urlopen(
     'https://www.nsstc.uah.edu/data/msu/v6.0/tlt/uahncdc_lt_6.0.txt')
 data = response.read()
-print(data)
+
 
 # Write data to file
 filename = "./global_temp/file/global.txt"
